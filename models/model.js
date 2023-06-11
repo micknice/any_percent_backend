ba8c29fe3f3f2750388be584253d5a22a11f13c1
mick nice <mikemccullagh@outlook.com>
2023-06-11 18:48:05 +0100

    schemas updated

--- models/model.js
+++ models/model.js
@@ -1,36 +1,55 @@
 const mongoose = require('mongoose');
 
-// const dataSchema = new mongoose.Schema({
-//     name: {
-//         required: true,
-//         type: String
-//     },
-//     age: {
-//         required: true,
-//         type: Number
-//     }
-// })
+const userSchema = new mongoose.Schema({
+    userUID: {
+        required: true,
+        type: String
+    },
+    userName: {
+        required: true,
+        type: String
+    },
+    userTrainingLog: {
+        required: true,
+        type: [sessionSchema]
+    },
+    defaultWeightUnit: {
+        required: true,
+        type: String
+    }
+    
+})
 
-// const sessionSchema = new mongoose.Schema({
-//     userId: {
-//         required: true,
-//         type: Number
-//     },
-//     date: {
-//         required: true,
-//         type: String
-//     },
-//     exercises: {
-//         required: true,
-//         type: []
+const sessionSchema = new mongoose.Schema({
+    userId: {
+        required: true,
+        type: Number
+    },
+    date: {
+        required: true,
+        type: String
+    },
+    exercises: {
+        required: true,
+        type: []
 
-//     },
-//     sets: {
-//         required: true,
-//         type: []
-//     } 
+    },
+    sets: {
+        required: true,
+        type: []
+    } 
 
-// })
+})
+const liftSchema = new mongoose.Schema({
+    lift: {
+        required: true,
+        type: exerciseSchema
+    },
+    sets: {
+        required: true, 
+        type: [setSchema]
+    }
+})
 
 const exerciseSchema = new mongoose.Schema({   
     name: {
@@ -55,20 +74,32 @@ const exerciseSchema = new mongoose.Schema({
     }
 })
 
-// const setSchema = new mongoose.Schema({
-//     exerciseName: {
-//         required: true,
-//         type: String
-//     },
-//     reps: {
-//         required: true,
-//         type: Number
-//     },
-//     setType: {
-//         required: true,
-//         type: String
-//     }
-// })
+const setSchema = new mongoose.Schema({
+    exerciseName: {
+        required: true,
+        type: String
+    },
+    weight: {
+        required: true,
+        type: Number
+    },
+    weightUnit: {
+        required: true,
+        type: String
+    },    
+    repsPerformed: {
+        required: true,
+        type: Number
+    },
+    targetReps: {
+        required: false,
+        type: Number
+    },
+    setType: {
+        required: true,
+        type: String
+    }
+})
 
 const Exercise = mongoose.model('Exercise', exerciseSchema)
 
