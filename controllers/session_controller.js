e5539424fa9dba726983ae712dcee12f1361bfad
mick nice <mikemccullagh@outlook.com>
2023-06-12 23:42:24 +0100

    commit

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,45 +1,34 @@
 const app = require("../app");
-const {
-  fetchSessionsByUID,
-  insertNewSession,
-  updateSessionWithStack,
+const {fetchSessionsByUID,insertNewSession,updateSessionWithStack,
 } = require("../models/session_model");
 const multer = require("multer");
 const upload = multer();
 
 const getSessionsByUID = (req, res, next) => {
-  const { UID } = req.params;
-  return fetchSessionsByUID(UID)
+    const { UID } = req.params;
+    return fetchSessionsByUID(UID)
     .then((result) => {
-      res.status(200).send({ sessions: result });
+    res.status(200).send({ sessions: result });
     })
-    .catch((err) => {
-      next(err);
-    });
+    .catch((err) => {next(err)});
 };
 
 const postNewSession = (req, res, next) => {
-  const { UID } = req.params;
-  return insertNewSession(UID)
-    .then((result) => {
-      console.log(result);
-      res
-        .status(201)
-        .send({ msg: "new session created", sessionId: result.sessionId });
+    const { UID } = req.params;
+    return insertNewSession(UID)
+    .then((result) => {     
+    res.status(201)
+    .send({ msg: "new session created", sessionId: result.sessionId });
     })
-    .catch((err) => {
-      next(err);
-    });
+    .catch((err) => {next(err)});
 };
 const patchSessionWithStack = (req, res, next) => {
-  const { sessionId, lift } = req.body;
-  return updateSessionWithStack(sessionId, lift)
+    const { sessionId, lift } = req.body;
+    return updateSessionWithStack(sessionId, lift)
     .then((result) => {
-      res.status(201).send({ msg: "Session updated", stack: result });
+    res.status(201).send({ msg: "Session updated", stack: result });
     })
-    .catch((err) => {
-      next(err);
-    });
+    .catch((err) => {next(err)});
 };
 
 module.exports = { getSessionsByUID, postNewSession, patchSessionWithStack };
